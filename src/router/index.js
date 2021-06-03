3f146bda2b6f198e7241b1ad1506af30acd65f73
fadeds <1154250793@qq.com>
2021-06-03 17:05:32 +0800

    修改路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -10,10 +10,11 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-];
+]
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
-});
+})
 
-export default router;
+export default router
