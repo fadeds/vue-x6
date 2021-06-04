f022270ac630f40db30b35096d8fd5551efc2411
fadeds <1154250793@qq.com>
2021-06-04 09:13:30 +0800

    新建分支

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const ENV = process.env.NODE_ENV
 
 module.exports = {
-  publicPath: ENV === 'development' ? '' : '/vue_x6',
-  outputDir: 'vue_x6',
+  publicPath: ENV === 'development' ? '' : '/vue_x6/',
+  // outputDir: 'vue_x6',
 }
