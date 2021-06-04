a9a7f342840dfd2c6adfc25717d95b9b0bceaf5a
fadeds <1154250793@qq.com>
2021-06-04 09:32:55 +0800

    修改路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const ENV = process.env.NODE_ENV
 
 module.exports = {
-  publicPath: ENV === 'development' ? '' : '/resume-vue',
+  publicPath: ENV === 'development' ? '' : '/vue-x6',
   // outputDir: 'vue_x6',
 }
